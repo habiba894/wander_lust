173f3de6fa2072703062712cad7d3b3f5b4cc7e2
habiba894 <habibamohamed162004@gmail.com>
2026-06-25 07:30:56 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/wander_lust/" : "/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
